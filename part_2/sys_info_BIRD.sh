ad81b4d9b84a1977b466e3fce3959bebc1f6f2a9
Gwendolyn <birdoptera@gmail.com>
2019-02-12 10:52:36 -0600

    Using echo shows that the commands are working. Now to figure out how to write to a file.

--- part_2/sys_info_BIRD.sh
+++ part_2/sys_info_BIRD.sh
@@ -1,11 +1,12 @@
 #!/bin/BASH
 
 
-date +"%c"
+#date +"%c"
 timeanddate=$(date)
-
+echo "The time and date is $timeanddate"
 loggedusers=$(who)
-
+echo "$loggedusers are current logged in users"
 sys_uptime=$(uptime)
+echo "$sys_uptime is the system uptime"
 
 
