0594ae4f25dda2c407d383587bf7f8f0f2d947b1
Gwendolyn <birdoptera@gmail.com>
2019-02-12 10:11:18 -0600

    Actually, this is the unmodified file. The previous one is the one I fixed before I realized I had to commit all my fixes because I am not good at following directions #ADDlife

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -1,30 +1,6 @@
 #! /bin/bash/python3
 
 # Reading data from the user's input
-
-a = input("Enter a : ")
-
-print("You entered", a, "which is a", type(a))
-
-b = input("Enter b : ")
-
-print("You entered", b, "which is a", type(b))
-
-
-#######################################################################
-# HINT: why would we be checking what type a and b are again?
-#       Let's assume we want a and b to be integers at this point
-#######################################################################
-a = int(a)
-b = int(b)
-
-print("What is", a, "now?", type(a))
-print("What is", b, "now?", type(a))
-
-
-#! /bin/bash/python3
-
-# Reading data from the user's input
 a = input("Enter a : ")
 
 print("You entered", a, "which is a", type(a))
@@ -70,29 +46,6 @@ print("a ** b =", power, ", which is a", type(power))
 
 a += 1
 print("Incrementing \"a\" by one results in", a, "which is a", type(a))
-total = a + b
-print("a + b =", total, ", which is a", type(total))
-
-difference = a - b
-print("a - b =", difference, ", which is a", type(difference))
-
-product = a * b
-print("a * b =", product, ", which is a", type(product))
-
-quotient = a / b
-print("a / b =", quotient, ", which is a", type(quotient))
-
-floor_quotient = a // b
-print("a // b =", floor_quotient, ", which is a",type(floor_quotient),)
-
-remainder = a % b
-print("a % b =", remainder, ", which is a", type(remainder))
-
-power = a ** b
-print("a ** b =", power, ", which is a", type(power))
-
-a += 1
-print("Incrementing \"a\" by one results in", a, "which is a", type(a))
 
 b -= 1
 print("Decrementing \"b\" by one results in", b, "which is a", type(b))
@@ -106,12 +59,12 @@ print("Now \"b\" equals", b, "and is a", type(b))
 
 
 total = a + b
-print("a + b =", total, ", which is a", type(total))
+ print("a + b =", total, ", which is a", type(total))
 
 difference = a - b
 print("a - b =", difference, ", which is a", type(difference))
 
-product = a * b
+    product = a * b
 print("a * b =", product, ", which is a", type(product))
 
 quotient = a / b
@@ -121,10 +74,14 @@ floor_quotient = a // b
 print("a // b =", floor_quotient, ", which is a", type(floor_quotient))
 
 remainder = a % b
-print("a % b =", remainder, ", which is a,", type(remainder))
+print("a % b =," remainder ", which is a," type(remainder))
 
 power = a ** b
-print("a ** b =", power, ", which is a", type(power))
+print("a ** b =",
+        power,
+        ", which is a",
+        type(power),
+        )
 
 a += 1
 print("Incrementing \"a\" by one results in", a, "which is a", type(a))
